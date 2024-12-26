a60cb66dcf399d19e0a847d6ce33c21cd3591752
DrakoGer <gije.ger@gmail.com>
2024-12-26 19:48:05 +0400

    Добавлен gitignore

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Yura on 23.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
