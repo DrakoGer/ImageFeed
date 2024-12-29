6c850784eab76f3611f309c31f40783ec2507bbf
DrakoGer <gije.ger@gmail.com>
2024-12-30 01:38:13 +0400

    готово

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -5,6 +5,14 @@
 //  Created by Yura on 28.12.24.
 //
 
+import UIKit
+
 final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
     
 }
